0834277b1ae2c1826dd2b73fe08efa1e4dfe996f
test1 <414319287@qq.com>
2022-04-25 05:23:52 +0800

    Del
    test

--- src/nginxconfig/i18n/zh-cn/templates/callouts/droplet.js
+++ src/nginxconfig/i18n/zh-cn/templates/callouts/droplet.js
@@ -1,6 +1,6 @@
 
 
 export default {
-    lookingForAPlaceToDeploy: '👋 在寻找部署新配置的地方？',
-    tryOutDigitalOceansDroplet: '试用已安装 NGINX 的 DigitalOcean LEMP Droplet。',
+    // lookingForAPlaceToDeploy: '👋 在寻找部署新配置的地方？',
+    // tryOutDigitalOceansDroplet: '试用已安装 NGINX 的 DigitalOcean LEMP Droplet。',
 };
