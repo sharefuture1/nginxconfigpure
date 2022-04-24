4a013e9973fcbc5c97e703126c6378239cb3a2e2
test1 <414319287@qq.com>
2022-04-25 05:36:08 +0800

    Del:pure

--- src/nginxconfig/templates/footer.vue
+++ src/nginxconfig/templates/footer.vue
@@ -6,32 +6,32 @@
             <p>
                 <a href="#top" class="button is-primary is-small">{{ $t('templates.footer.backToTop') }}</a>
             </p>
-            <p>
-                {{ $t('templates.footer.thisToolIs') }}
-                <ExternalLink
-                    :text="$t('templates.footer.openSourceOnGitHub')"
-                    link="https://nav.young1.ml"
-                ></ExternalLink>
-                {{ $t('templates.footer.underThe') }}
-                <ExternalLink
-                    :text="$t('templates.footer.mit')"
-                    link="https://nav.young1.ml"
-                ></ExternalLink>
-                {{ $t('templates.footer.license') }}
-                {{ $t('templates.footer.weWelcomeFeedbackAndContributions') }}
-            </p>
-            <p>
-                {{ $t('templates.footer.originallyCreatedBy') }}
-                <ExternalLink
-                    :text="$t('templates.footer.balintSzekeres')"
-                    link="https://nav.young1.ml"
-                ></ExternalLink>,
-                {{ $t('templates.footer.maintainedBy') }}
-                <ExternalLink
-                    :text="$t('templates.footer.digitalOcean')"
-                    link="https://nav.young1.ml"
-                ></ExternalLink>.
-            </p>
+            // <p>
+            //     {{ $t('templates.footer.thisToolIs') }}
+            //     <ExternalLink
+            //         :text="$t('templates.footer.openSourceOnGitHub')"
+            //         link="https://nav.young1.ml"
+            //     ></ExternalLink>
+            //     {{ $t('templates.footer.underThe') }}
+            //     <ExternalLink
+            //         :text="$t('templates.footer.mit')"
+            //         link="https://nav.young1.ml"
+            //     ></ExternalLink>
+            //     {{ $t('templates.footer.license') }}
+            //     {{ $t('templates.footer.weWelcomeFeedbackAndContributions') }}
+            // </p>
+            // <p>
+            //     {{ $t('templates.footer.originallyCreatedBy') }}
+            //     <ExternalLink
+            //         :text="$t('templates.footer.balintSzekeres')"
+            //         link="https://nav.young1.ml"
+            //     ></ExternalLink>,
+            //     {{ $t('templates.footer.maintainedBy') }}
+            //     <ExternalLink
+            //         :text="$t('templates.footer.digitalOcean')"
+            //         link="https://nav.young1.ml"
+            //     ></ExternalLink>.
+            // </p>
         </div>
     </div>
 </template>
