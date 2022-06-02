a4cd98897a750512cf0d386c36835ffbfb3735dc
selfstudy1 <102909589+qtest3@users.noreply.github.com>
2022-06-02 15:19:53 +0800

    Revert "Del"
    This reverts commit 0834277b1ae2c1826dd2b73fe08efa1e4dfe996f.

--- src/nginxconfig/i18n/zh-cn/templates/callouts/contribute.js
+++ src/nginxconfig/i18n/zh-cn/templates/callouts/contribute.js
@@ -1,6 +1,6 @@
 
 
 export default {
-    wantToContributeChanges: '更多工具',
-    getInvolvedOnGitHub: '欢迎使用',
+    wantToContributeChanges: '👋 想要申请新的功能，提出修改意见，或将该工具翻译成新的语言？',
+    getInvolvedOnGitHub: '欢迎在 Github 中参与本项目',
 };
